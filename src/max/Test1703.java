d3a47351b6123a36016fb3f1c27594a11a101bfa
Mskanda <mskanda@student.tgm.ac.at>
2017-03-28 12:01:57 +0200

    Max mit Smiley

--- src/max/Test1703.java
+++ src/max/Test1703.java
@@ -2,7 +2,7 @@ package max;
 
 public class Test1703 {
 
-	public static void grüßen(){
-		System.out.println("Hallo Max!");
+	public static void grï¿½ï¿½en(){
+		System.out.println("Hallo Max :)");
 	}
 }
